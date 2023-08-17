b217f123a0ba037041a625648e2d87cb46679625
StephenStolk <134587780+StephenStolk@users.noreply.github.com>
2023-08-17 23:52:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ cp = cv.VideoCapture("TestVid.mp4")
 
 ht = int(cp.get(cv.CAP_PROP_FRAME_HEIGHT))
 wt = int(cp.get(cv.CAP_PROP_FRAME_WIDTH))
-fps = cap.get(cv2.CAP_PROP_FPS)
+fps = cp.get(cv2.CAP_PROP_FPS)
 
 path = "SlowedVideo.mp4"
 fcc = cv.VideoWriter_fourcc(*'mp4v')
