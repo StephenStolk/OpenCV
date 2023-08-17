a202c0ef1dd28192ea52a580a94f3334bc453206
StephenStolk <134587780+StephenStolk@users.noreply.github.com>
2023-08-17 23:50:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 import cv2 as cv
 
-print(cv.__version__)
+cp = cv.VideoCapture("TestVid.mp4")
+
+ht = int(cp.get(cv.CAP_PROP_FRAME_HEIGHT))
+wt = int(cp.get(cv.CAP_PROP_FRAME_WIDTH))
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+path = "SlowedVideo.mp4"
+fcc = cv.VideoWriter_fourcc(*'mp4v')
+
+output = cv.VideoWriter(path, fcc, 2,
+						(wit, ht))
+
+while True:
+	ret, frame = cp.read()
+	cv2.imshow("frame", frame)
+	output.write(frame)
+	k = cv.waitKey(24)
+	if k == ord("q"):
+		break
+
+cp.release()
+output.release()
+cv.destroyAllWindows()
